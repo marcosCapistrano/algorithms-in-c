6931192ee0ed7cb607d06390d31f5144fbadeafc
Marcos Capistrano <mcapistrano50@gmail.com>
2024-11-24 13:41:43 -0300

    Stack implementation, using arrays and linked lists

--- data-structures/elementary/stack.c
+++ data-structures/elementary/stack.c
@@ -1,6 +1,37 @@
+/*
+    The first half of this program uses an array to implement a stack, simple if the max size of the stack is known.
+    The second half is implemented using a linked list, more complicated but allows for dynamic size.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ARRAY_IMPLEMENTATION 1
+
+#ifdef ARRAY_IMPLEMENTATION
+
+#define MAX 100
+
+static int stack[MAX+1], p;
+
+void Stack_init()
+{
+    p = 0;
+}
+
+void Stack_push(int v)
+{
+    stack[p++] = v;
+}
+
+int Stack_pop()
+{
+    return stack[--p];
+}
+
+#endif
+
+
+#ifdef LINKEDLIST_IMPLEMENTATION
 typedef struct Node
 {
     int key;
@@ -53,3 +84,4 @@ int main()
     printf("Currently on stack: %d\n", Stack_pop(&sp));
     printf("Currently on stack: %d\n", sp->key);
 }
+#endif
